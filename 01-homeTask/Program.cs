b905485bd86deacb2b35e21811343b81c056f183
RavanM01 <ravanvm-ap116@code.edu.az>
2024-10-01 20:21:26 +0400

    HomeWork Updated

---
+++ 01-homeTask/Program.cs
@@ -0,0 +1,67 @@
+﻿//Murekkeb or sade 
+/*
+int n = 10;
+int sum = 0;
+int i = 1; 
+//For version
+
+for (int i = 1; i <= n; i++)
+{
+    if (n % i == 0)
+    {
+        sum += 1;
+    }
+}
+
+if (sum > 2)
+{
+    Console.WriteLine("Murekkebdir");
+}
+else
+{
+    Console.WriteLine("sadedir");
+}
+//-------------------------------------------
+//While version
+while (i <= n)
+{
+    if (n % i == 0)
+    {
+        sum += 1;
+    }
+    i++;
+}
+if (sum > 2)
+{
+    Console.WriteLine("Murekkebdir");
+}
+else
+{
+    Console.WriteLine("sadedir");
+}
+*/
+//--------------------------------------------
+//Factorial hesablama
+/*
+int n = 25;
+int fact = 1;
+while (n>=1)
+{
+    fact *= n;
+    n--;
+
+}
+Console.WriteLine(fact);
+*/
+//-------------------------------------------
+//Ededin reqemlerinin cemi
+int n = 267;
+int sum = 0;
+
+while (n != 0)
+{
+    sum += n % 10; 
+    n /= 10; 
+}
+
+Console.WriteLine(sum);
