b525d8dbf72a449b477af2f56c9701c603661c58
RavanM01 <ravanvm-ap116@code.edu.az>
2024-10-08 20:17:55 +0400

    allTasks Updated

---
+++ HomeTask(8.10.24)/MinAndSpaceCleaner/MinAndSpaceCleaner/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MinChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 34,56,123,5676,12, 2, 3 };
+            Console.WriteLine(MinChecker(ints));
+        }
+        static int MinChecker(int[] ints) {
+            int min = ints[0];
+
+            foreach (int i in ints)
+            {
+                
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+
+            return min;
+
+        }
+    }
+}
---
+++ HomeTask(8.10.24)/StringFormatter/StringFormatter/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace StringFormatter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string helo = "Hello, Wo rl d!";
+            BigLetter(helo);
+            TextFormatter(helo);
+        }
+        static void BigLetter(string text)
+        {
+            string Bigchar = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (" " == text[i].ToString())
+                {
+                    Bigchar += text[i + 1]+" ";
+                }
+            }
+            Console.WriteLine(Bigchar);
+
+        }
+
+        static void TextFormatter(string text)
+        {
+            string chars = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (" " != text[i].ToString())
+                {
+                    chars += text[i].ToString();
+                }
+            }
+            Console.WriteLine(chars);
+        }
+
+    }
+}
