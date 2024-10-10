2b9971ee8a8775b883e36f52d78ebb1905ec9c27
RavanM01 <ravanvm-ap116@code.edu.az>
2024-10-11 01:13:09 +0400

    HomeTask Updated

---
+++ HomeTask(10.10.24)/İnsertArray/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace İnsertArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = { 23, 654, 4, 234, 87, 9 };
+            Console.WriteLine("Arrayin Ilk uzunlugu: "+ Array.Length);
+            İnsertArray(ref Array,45,23,35,76,38,95);
+            Console.WriteLine("\n"+"Arrayin Son uzunlugu: " + Array.Length);
+            
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine("\n"+$"Arrayin {i+1} elementi: "+Array[i]);
+            }
+            
+
+        }
+        static void İnsertArray(ref int[] array, params int[] arr1)
+        {
+            int j = 0;
+            int[] arr= new int[arr1.Length+array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                arr[i] = array[i];               
+            }
+            for (int i = array.Length; i < arr.Length; i++)
+            {               
+                    arr[i] = arr1[j];
+                    j++;                
+            }
+
+            array = arr;
+        }
+
+
+        
+    }
+}
