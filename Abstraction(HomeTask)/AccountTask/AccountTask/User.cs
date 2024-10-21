86b3494f81538ce8afef6e55ad5724820fcdedc8
RavanM01 <ravanvm-ap116@code.edu.az>
2024-10-22 00:24:45 +0400

    Tasks Completed

---
+++ Abstraction(HomeTask)/AccountTask/AccountTask/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AccountTask
+{
+    internal class User : IAccount
+    {
+        private static int _Id = 1;
+        private readonly int _id;
+
+        public User(string fullName, string email, string password)
+        {
+            _id = _Id; 
+            _Id++;
+            FullName = fullName;
+            Email = email;
+            Password = password;
+        }
+
+        public int Id { get { return _id; } }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public bool PasswordChecker(string password)
+        {
+            /*
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$";
+
+            // Regex ilə şərtləri yoxlayırıq
+            return Regex.IsMatch(password, pattern);
+
+            */
+            
+            if (password.Length>=8 && password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit)) 
+            {
+                return true;
+            }
+            return false;
+                    
+        }
+
+
+        public void ShowInfo(User user)
+        {
+            Console.WriteLine($"User ID:{user.Id}\nFullName:{user.FullName}\nEmail:{user.Email}");
+        }
+    }
+}
---
+++ Abstraction(HomeTask)/StudentGroup/StudentGroup/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1=new Student("Ravan",88.3);
+            Student student2=new Student("Hikmat",67.4);
+            Student student3=new Student("akram",37.9);
+            Group group = new Group("AB107",12);
+            group.AddStudent(student1);
+            group.AddStudent(student2);
+            group.AddStudent(student3);
+            Console.WriteLine(group.CheckGroupNo("AB107")+"\n");
+            student1.StudentInfo();
+            group.GetStudent(3);
+            group.GetAllStudents();
+        }
+    }
+}
